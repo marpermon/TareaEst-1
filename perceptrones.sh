ba358ec2768b1a1f82d8e8e200d1b974baad79df
marpermon <93532231+marpermon@users.noreply.github.com>
2024-04-29 11:17:56 -0600

    Update perceptrones.sh

--- perceptrones.sh
+++ perceptrones.sh
@@ -8,7 +8,11 @@ globalH=(2 6 8 16 20)
 for bit in "${BitsPC[@]}"; do
     # Iterate over the elements of the second array
     for hist in "${globalH[@]}"; do
-        # Execute the Python script with arguments
-        python3 branch_predictor.py --bp 3 -n "$bit" -g "$hist"
+        start=$(date +%s.%N)
+        python3 branch_predictor.py --bp 3 -n "$bit" -g "$hist" | grep "% predicciones correctas:">> output.txt
+        end=$(date +%s.%N)
+        execution_time=$(echo "$end - $start" | bc)
+        echo "PCbits: $bit , global hist: $hist Execution time: $execution_time seconds">> output.txt
+        echo "--">> output.txt
     done
 done
