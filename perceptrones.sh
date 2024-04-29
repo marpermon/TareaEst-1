53964aac0f330a75ab07fade228a2c9dfaa524b2
marpermon <93532231+marpermon@users.noreply.github.com>
2024-04-29 10:33:44 -0600

    Create perceptrones.sh

---
+++ perceptrones.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Define two arrays
+BitsPC=(4 8 12 16 20)
+globalH=(2 6 8 16 20)
+
+# Iterate over the elements of the first array
+for bit in "${BitsPC[@]}"; do
+    # Iterate over the elements of the second array
+    for hist in "${globalH[@]}"; do
+        # Execute the Python script with arguments
+        python branch_predictor.py -n "$bit" -g "$hist"
+    done
+done
