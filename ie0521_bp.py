72f1aa5706bfc3d73eda03700c2a9f71448dbcef
marpermon <93532231+marpermon@users.noreply.github.com>
2024-05-02 22:38:35 -0600

    archivo aceptable

--- ie0521_bp.py
+++ ie0521_bp.py
@@ -1,13 +1,14 @@
 class ie0521_bp:
     def __init__(self):
-        #Escriba aquí el init de la clase
-        self.global_history_size = 7
-        self.global_history_reg = "0"*self.global_history_size
-        self.tag_1Hist_size=7
+        
+        self.global_history_size = 5
+        self.global_history_reg = '0'*self.global_history_size
+        self.tag_1Hist_size=5
         self.size_of_tag_1=2**(self.tag_1Hist_size)
-        self.tag_1=[0 for i in range(self.size_of_tag_1)]
-
-        self.bits_to_index = 7
+        self.tag_1=['0' for i in range(self.size_of_tag_1)]
+        self.lnt=len(str(2**self.tag_1Hist_size))
+        #self.historia_fila=["x" for i in range(self.tag_1Hist_size)]
+        self.bits_to_index = 5
         self.size_of_bimodal = 2**self.bits_to_index
         self.bimodal = [0 for i in range(self.size_of_bimodal)]
         
@@ -37,21 +38,22 @@ class ie0521_bp:
 
     def predict(self, PC):
         
-        PC_index = int(PC) % self.tag_1Hist_size
+        PC_index = int(PC) % self.size_of_tag_1
         tag_index = int(self.global_history_reg[-self.tag_1Hist_size:],2)
         hashFunc = PC_index ^ tag_index
         match=0
         fila=0
         for i in range(self.size_of_tag_1):#recorremos la tabla
-            if self.tag_1[i]!= 0: #primero vemos si la fila tiene algo, osea, que no sea igual a int(0)
-                if int(self.tag_1[i][:self.tag_1Hist_size]) == hashFunc: #vemos si el tag está en la fila
+            if self.tag_1[i]!= '0': #primero vemos si la fila tiene algo, osea, que no sea igual a sólo un 0
+                if int(self.tag_1[i][:self.lnt]) == hashFunc: #vemos si el tag está en la fila
                     match+=1 #ESO SIGNIFICA QUE HICIMOS MATCH
                     fila = i
-                    # a la par del tag va a ir un dígito decimal de 3 bits que será el ctr
-                    if int(self.tag_1[i][self.tag_1Hist_size])<4:
+                    # a la par del tag va a ir un dígito decimal de 3 bits que será el self.lnt
+                    if int(self.tag_1[i][self.lnt])<4:
                         prediction = "N"
                     else:
                         prediction = "T"
+                    break
                         
         if match==0: #si no hubo match nos pasamos al bimodal
             bimodal_entry = self.bimodal[PC_index]
@@ -61,13 +63,18 @@ class ie0521_bp:
             else:
                 prediction = "T"
         
-        return [prediction, match, fila]
+        return prediction+"{}{}".format(match,fila)
             
         """if int(self.tag_1[i][self.tag_1Hist_size]) in range(1,8):
     #para que no se sature el contador int(self.tag_1[i][self.tag_1Hist_size])"""
 
-    def update(self, PC, result, prediction, match, fila):
+    def update(self, PC, result, pred):
         #siempre se actualiza el bimodal
+
+        prediction=pred[0]
+        match=int(pred[1])
+        fila=int(pred[2:])
+        
         PC_index = int(PC) % self.size_of_bimodal
         if prediction == "T" and self.bimodal[PC_index]<3:#creamos saturación
                 self.bimodal[PC_index]+=1
@@ -75,25 +82,26 @@ class ie0521_bp:
                 self.bimodal[PC_index]-=1
         
         if match:
-            if prediction == "T" and int(self.tag_1[fila][self.tag_1Hist_size])<7:#creamos saturación
-                self.tag_1[fila]=self.tag_1[fila][:self.tag_1Hist_size]+str(int(self.tag_1[fila][self.tag_1Hist_size])+1)
-            elif prediction == "N" and int(self.tag_1[fila][self.tag_1Hist_size])>0:
-                self.tag_1[fila]=self.tag_1[fila][:self.tag_1Hist_size]+str(int(self.tag_1[fila][self.tag_1Hist_size])-1)
+            if result == "T" and int(self.tag_1[fila][self.lnt])<4:#creamos saturación
+                self.tag_1[fila]=self.tag_1[fila][:self.lnt]+str(int(self.tag_1[fila][self.lnt])+1)
+            elif result == "N" and int(self.tag_1[fila][self.lnt])>0:
+                self.tag_1[fila]=self.tag_1[fila][:self.lnt]+str(int(self.tag_1[fila][self.lnt])-1)
         #en la tabla no podemos reemplazar un digito porque las strings no lo permiten, debemos reemplazar toda la string
         else:
-            PC_index = int(PC) % self.tag_1Hist_size
+            PC_index = int(PC) % self.size_of_tag_1
             tag_index = int(self.global_history_reg[-self.tag_1Hist_size:],2)
             hashFunc = PC_index ^ tag_index
+            
             for i in range(self.size_of_tag_1):#recorremos la tabla
-                if self.tag_1[i]== 0:
-                    self.tag_1[i]=str(hashFunc)+"0"
+                if self.tag_1[i]== '0':
+                    self.tag_1[i]='0'*(self.lnt-len(str(hashFunc)))+str(hashFunc)+'0'
                     break
                 
         self.global_history_reg = self.global_history_reg[-self.global_history_size+1:] #eliminamos la primera
         if result == "T":
-            self.global_history_reg+="1"
+            self.global_history_reg+='1'
         else:
-            self.global_history_reg+="0"
+            self.global_history_reg+='0'
         #print("GHR = "+self.global_history_reg)
 
         #Update stats
